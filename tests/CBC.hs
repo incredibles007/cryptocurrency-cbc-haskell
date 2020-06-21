140746b8f9cb49a8e7a057e86fe4b4df18464b55
incredible <luckyckgp007@gmail.com>
2020-06-21 16:14:10 +0200

    Test segment with negative offset

--- tests/CBC.hs
+++ tests/CBC.hs
@@ -7,11 +7,13 @@ import Crypto.CBC
 import qualified Data.ByteString as B
 import Imports
 
-prop_segment :: ArbitraryBS0_2901 -> NonNegative Int -> Int0_131 -> Bool
-prop_segment (ArbitraryBS0_2901 bs) (NonNegative offset) (Int0_131 len) =
-    let result = B.take len (B.drop offset bs)
-        extra  = B.replicate (len - B.length result) 0
-     in (result `B.append` extra) `propertyEq` segment bs offset len
+prop_segment :: ArbitraryBS0_2901 -> Int -> Int0_131 -> Bool
+prop_segment (ArbitraryBS0_2901 bs) offset (Int0_131 len)
+    | offset < 0 = B.replicate len 0 `propertyEq` segment bs offset len
+    | otherwise  =
+        let result = B.take len (B.drop offset bs)
+            extra  = B.replicate (len - B.length result) 0
+         in (result `B.append` extra) `propertyEq` segment bs offset len
 
 tests :: TestTree
 tests = testGroup "cbc"
